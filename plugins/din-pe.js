76ea09128cbf457d43702e2ebde8f09735858b60
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2024-03-30 18:52:27 -0500

    Update din-pe.js

--- plugins/din-pe.js
+++ plugins/din-pe.js
@@ -8,12 +8,12 @@ return console.log('La página no esta funcionando actualmente')
 return response.json()
 }).then(data => {
 const formattedResult = `
-🔰 |*DNI:* ${text}
-🎯|*NOMBRE:* ${data.nombres}
-🔴|*APELLIDO PATERNO:* ${data.apellidoPaterno}
-📊|*APELLIDO MATERNO:* ${data.apellidoMaterno}
+*🔰|DNI:* ${text}
+*🎯|NOMBRE:* ${data.nombres}
+*🔴|APELLIDO PATERNO:* ${data.apellidoPaterno}
+*📊|APELLIDO MATERNO:* ${data.apellidoMaterno}
 
->BY 😎 HACHEJOTA😎
+> BY 😎 HACHEJOTA😎
 `
 m.reply(formattedResult.trim())
 }).catch(error => {
